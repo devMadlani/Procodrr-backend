5b5016f4257cf2a0b1942859ccfdbf5952a4c1e8
Dev Madlani <madlanidev@gmail.com>
2025-05-16 21:04:42 +0530

    crud operation in nodejs

--- section-12 {database}/22_mongodb-in-nodejs/app.js
+++ section-12 {database}/22_mongodb-in-nodejs/app.js
@@ -1,18 +1,48 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 
 const client = new MongoClient("mongodb://localhost:27017");
 await client.connect();
 
-const admin = client.db().admin();
-const dbList = await admin.listDatabases();
-console.log(dbList);
+const db = client.db("school");
+// console.log(await db.listCollections().toArray());
 
-// const db = client.db("todoApp");
-// console.log(db.namespace);
+const studentsCollection = db.collection("students");
+const teachersCollection = db.collection("teachers");
 
-// const collection = db.collection("todos");
-// const result = await collection.find().toArray();
+//create
+// const result1 = await studentsCollection.insertOne({
+//   name: "Dev Madlani",
+//   age: 22,
+// });
+// const result2 = await teachersCollection.insertMany([
+//   { name: "Disha Madlani", age: 23 },
+//   { name: "Darshu Madlani", age: 24 },
+// ]);
 
-// console.log(result);
+//delete
+// console.log(studentsCollection.drop());
+
+// const a = await teachersCollection.deleteOne({
+//   _id: new ObjectId("682754e07da2dc1a759de78b"),
+// });
+
+// await teachersCollection.updateOne(
+//   { _id: new ObjectId("682754e07da2dc1a759de78b") },
+//   { $unset: { name: "" } }
+// );
+
+// await db.dropDatabase();
+
+//update
+
+const updateResult = await studentsCollection.updateOne(
+  { name: "Dev Madlani" },
+  { $set: { age: 25 } }
+);
+
+// const updateResult = await studentsCollection.replaceOne(
+//   { age: 23 },
+//   { name: "Dev Madlani" }
+// );
 
 client.close();
