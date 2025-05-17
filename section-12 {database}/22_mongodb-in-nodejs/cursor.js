0f0c767c242151a70076f02b8894b169a228854f
Dev Madlani <madlanidev@gmail.com>
2025-05-17 07:16:27 +0530

    about cursor

---
+++ section-12 {database}/22_mongodb-in-nodejs/cursor.js
@@ -0,0 +1,43 @@
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient("mongodb://localhost:27017");
+await client.connect();
+
+const db = client.db("todoApp");
+const collection = db.collection("todos");
+// const collection = db.collection("users");
+
+// const data = [];
+
+// for (let i = 0; i < 2; i++) {
+//   data.push({ title: `user ${i}`, isActive: i % 2 === 0 });
+// }
+
+// await collection.insertMany(data);
+
+// console.log(await collection.countDocuments());
+
+// Cursor
+
+const cursor = collection.find();
+
+console.log(await cursor.hasNext());
+
+let count = 0;
+while (await cursor.hasNext()) {
+  count++;
+  console.log(await cursor.next());
+  if (count === 5) break;
+}
+
+console.log(await cursor.hasNext());
+
+const moreData = await cursor.toArray();
+
+console.log(moreData);
+
+// for await (const doc of cursor) {
+//   console.log(doc);
+// }
+
+client.close();
