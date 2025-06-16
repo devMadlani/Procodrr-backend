96ff3987649c5e47cfd18fef24197724ffbf6c77
Dev Madlani <madlanidev@gmail.com>
2025-06-16 08:14:16 +0530

    virtuals in mongoose

--- section-13 (MVC Architechture)/12_custom-validation/customValidation.js
+++ section-13 (MVC Architechture)/12_custom-validation/customValidation.js
@@ -10,8 +10,8 @@ import User from "./UserModel.js";
 // const user = await User.findById("684d7d88e08c5c44b1deadce").populate(
 //   "parentId"
 // );
-// If we only need to populate selected fields then use this
 
+// If we only need to populate selected fields then use this
 const user = await User.findById("684d7d88e08c5c44b1deadce").populate({
   path: "parentId",
   select: "name age -_id",
---
+++ section-13 (MVC Architechture)/17_what-are-virtuals-in-mongoose/UserController.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import User from "./UserModel.js";
+
+const user = await User.findOne({ email: "krishna@gmail.com" });
+
+// user.hobbiesStr = "writing, sleeping, coding";
+
+console.log(user.toObject());
+// console.log(user.emailDomain);
+// console.log(user.isAdult);
+// console.log(user.hobbies);
+// console.log(user.schema.virtuals); // Shows all virtuals
+
+// console.log(user.toJSON({ virtuals: true })); // Converts Document to normal js object without virtuals
+
+await mongoose.disconnect();
