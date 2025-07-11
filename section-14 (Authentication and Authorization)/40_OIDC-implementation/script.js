6e49c37b6dcc15b772ff0c3dffaae9f540f9224f
Dev Madlani <madlanidev@gmail.com>
2025-07-12 00:00:41 +0530

    open the popup to load the auth server

--- section-14 (Authentication and Authorization)/40_OIDC-implementation/script.js
+++ section-14 (Authentication and Authorization)/40_OIDC-implementation/script.js
@@ -1,15 +1,32 @@
-const code = new URLSearchParams(location.search).get("code");
+const button = document.querySelector("button");
 
 const clientId =
   "1095542063427-n6ii04rv6k0hp8u9uno1ir7j09jul1nf.apps.googleusercontent.com";
-const clientSecret = "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";
 const redirectUrl = "http://localhost:5500";
+const clientSecret = "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";
+const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&scope=openid email profile&redirect_uri=${redirectUrl}`;
 
-if (code) {
-  fetchIdToken();
+button.addEventListener("click", () => {
+  window.open(authUrl, "auth-popup", "width=500,height=600");
+});
+
+let hasFetchedToken = false;
+window.addEventListener("message", ({ data }) => {
+  if (!hasFetchedToken && data.code) {
+    hasFetchedToken = true;
+    fetchIdToken(data.code);
+  }
+});
+
+if (window.name === "auth-popup") {
+  const code = new URLSearchParams(location.search).get("code");
+  if (code) {
+    window.opener.postMessage({ code });
+    window.close();
+  }
 }
 
-async function fetchIdToken() {
+async function fetchIdToken(code) {
   const payload = `code=${code}&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUrl}&grant_type=authorization_code`;
   const response = await fetch("https://oauth2.googleapis.com/token", {
     method: "POST",
