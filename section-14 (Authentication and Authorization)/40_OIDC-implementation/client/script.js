9865931b9a83783d701b7700b4256eb1b498bb1f
Dev Madlani <madlanidev@gmail.com>
2025-07-13 14:42:50 +0530

    add user profile display and session management for OIDC authentication

--- section-14 (Authentication and Authorization)/40_OIDC-implementation/client/script.js
+++ section-14 (Authentication and Authorization)/40_OIDC-implementation/client/script.js
@@ -1,25 +1,19 @@
-const button = document.querySelector("button");
 const baseURL = "http://localhost:4000";
-const clientId =
-  "1095542063427-n6ii04rv6k0hp8u9uno1ir7j09jul1nf.apps.googleusercontent.com";
-const redirectUrl = "http://localhost:5500/callback.html";
-const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&scope=openid email profile&redirect_uri=${redirectUrl}`;
 
-button.addEventListener("click", () => {
-  window.open(authUrl, "auth-popup", "width=500,height=600");
-});
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const img = document.querySelector("#img");
 
-// let hasFetchedToken = false;
-window.addEventListener("message", async ({ data }) => {
-  if (data.code) {
-    const res = await fetch(`${baseURL}/auth/google/callback`, {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const resData = await res.json();
-    console.log(resData);
-  }
+const res = await fetch(`${baseURL}/profile`, {
+  credentials: "include",
 });
+if (res.status === 401) {
+  location.href = "./login";
+}
+const { data } = await res.json();
+console.log(data.picture);
+if (data) {
+  name.textContent = data.name;
+  email.textContent = data.email;
+  img.src = data.picture;
+}
--- section-14 (Authentication and Authorization)/40_OIDC-implementation/server/app.js
+++ section-14 (Authentication and Authorization)/40_OIDC-implementation/server/app.js
@@ -2,30 +2,65 @@ import express from "express";
 import cors from "cors";
 import { fetchUser } from "./services/GoogleAuthService.js";
 import { writeFile } from "fs/promises";
+import cookieParser from "cookie-parser";
 import users from "./userDB.json" with { type : "json"}
+import sessions from "./sessionDB.json" with { type : "json"}
 const app = express();
 const PORT = 4000;
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+    origin: "http://localhost:5500",
     credentials: true,
   })
 );
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.post("/auth/google/callback", async (req, res) => {
+  const { sid } = req.cookies;
+  const existinSession = sessions.find(({ sessionID }) => sessionID === sid);
+  if (existinSession) {
+    return res.json({ message: "Already logged in" });
+  }
   const { code } = req.body;
   const { sub, email, name, picture } = await fetchUser(code);
   const newUer = { id: sub, email, name, picture };
   const existinUser = users.find(({ id }) => id === sub);
   if (existinUser) {
+    const sessionID = crypto.randomUUID();
+    sessions.push({ sessionID, userId: sub });
+    await writeFile("./sessionDB.json", JSON.stringify(sessions, null, 2));
+    res.cookie("sid", sessionID, {
+      maxAge: 1000 * 60 * 60 * 24,
+      httpOnly: true,
+    });
     return res.status(200).json(newUer);
   }
   users.push(newUer);
   await writeFile("./userDB.json", JSON.stringify(users, null, 2));
+  const sessionID = crypto.randomUUID();
+  sessions.push({ sessionID, userId: sub });
+  await writeFile("./sessionDB.json", JSON.stringify(sessions, null, 2));
+  res.cookie("sid", sessionID, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+  });
   res.status(200).json(newUer);
+});
+
+app.get("/profile", (req, res) => {
+  const { sid } = req.cookies;
+  const existinSession = sessions.find(({ sessionID }) => sessionID === sid);
+  if (!existinSession) {
+    return res.status(401).json({ message: "Not logged in" });
+  }
+  const existinUser = users.find(({ id }) => id === existinSession.userId);
+  if (!existinUser) {
+    return res.status(404).json({ message: "User not founds" });
+  }
+  return res.status(200).json({ data: existinUser });
 });
 
 app.listen(PORT, () => {
