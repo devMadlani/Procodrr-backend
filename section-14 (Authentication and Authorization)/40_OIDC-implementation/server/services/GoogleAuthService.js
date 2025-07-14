4999a57912ba234f2fa7918317965c88da4f8b3c
Dev Madlani <madlanidev@gmail.com>
2025-07-15 00:16:32 +0530

    used google-auth-library to fetch id token

--- section-14 (Authentication and Authorization)/40_OIDC-implementation/server/services/GoogleAuthService.js
+++ section-14 (Authentication and Authorization)/40_OIDC-implementation/server/services/GoogleAuthService.js
@@ -1,23 +1,19 @@
 import { OAuth2Client } from "google-auth-library";
 
-const client = new OAuth2Client();
 export async function fetchUser(code) {
   const clientId = process.env.CLIENT_ID;
   const redirectUrl = "http://localhost:4000/auth/google/callback";
   const clientSecret = process.env.CLIENT_SECRET;
-  const payload = `code=${code}&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUrl}&grant_type=authorization_code`;
-  const response = await fetch("https://oauth2.googleapis.com/token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: payload,
-  });
 
-  const data = await response.json();
+  const client = new OAuth2Client({
+    clientId,
+    clientSecret,
+    redirectUri: redirectUrl,
+  });
+  const { tokens } = await client.getToken(code);
 
   const loginTicket = await client.verifyIdToken({
-    idToken: data.id_token,
+    idToken: tokens.id_token,
     audience: clientId,
   });
 
