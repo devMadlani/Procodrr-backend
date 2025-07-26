dea23c181fa3844ff3e6d998f3a7804cd2f52c0a
Dev Madlani <madlanidev@gmail.com>
2025-07-26 15:00:21 +0530

    implemeted getJSON and setJSON methods

--- section-16 (Redis)/8_redis-in-nodejs/app.mjs
+++ section-16 (Redis)/8_redis-in-nodejs/app.mjs
@@ -1,9 +1,11 @@
-import { createClient } from "redis";
+import redisClient from "./redis.mjs";
 
-const redisClient = await createClient().connect();
+// const result = await redisClient.setJSON("test", {
+//   name: "dev",
+//   age: 19,
+// });
 
-const result = await redisClient.set("name", "dev");
-
+const result = await redisClient.getJSON("test");
 console.log(result);
 
 redisClient.quit();
