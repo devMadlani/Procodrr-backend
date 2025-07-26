063757978e42ddf740b2c9f68660c4b0c0a5ea59
Dev Madlani <madlanidev@gmail.com>
2025-07-26 15:44:13 +0530

    working with redisjson

--- section-16 (Redis)/8_redis-in-nodejs/app.mjs
+++ section-16 (Redis)/8_redis-in-nodejs/app.mjs
@@ -5,7 +5,27 @@ import redisClient from "./redis.mjs";
 //   age: 19,
 // });
 
-const result = await redisClient.getJSON("test");
+// const result = await redisClient.json.arrPop("user:1", {
+//   path: "$.subjects",
+//   index: 0,
+// });
+
+// await redisClient.json.set("user:1", "$.address", {
+//   name: "dev",
+// });
+
+// const result = await redisClient.json.arrLen("user:1", {
+//   path: "$.subjects",
+// });
+
+// const result = await redisClient.json.get("user:1", {
+//   path: "$.subjects[*]",
+// });
+
+const result = await redisClient.json.get("user:1", {
+  path: "$..name",
+});
+
 console.log(result);
 
 redisClient.quit();
