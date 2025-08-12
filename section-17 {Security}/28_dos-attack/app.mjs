49eaad809f366980d9bae049eb54de230fb5c691
Dev Madlani <madlanidev@gmail.com>
2025-08-12 19:15:49 +0530

    implemented rate limit

--- section-17 {Security}/28_dos-attack/app.mjs
+++ section-17 {Security}/28_dos-attack/app.mjs
@@ -13,10 +13,42 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-app.get("/register", async (req, res) => {
-  bcrypt.hashSync("123456", 14);
-  return res.json({ message: "Registered Successfully" });
-});
+const rateLimitStore = {};
+
+function rateLimiter({ windowSize, numOfRequest }) {
+  return function (req, res, next) {
+    const currentTime = Date.now();
+    if (!rateLimitStore[req.ip]) {
+      rateLimitStore[req.ip] = {
+        startTime: currentTime,
+        count: 1,
+      };
+      return next();
+    }
+    if (currentTime - rateLimitStore[req.ip].startTime > windowSize) {
+      rateLimitStore[req.ip] = {
+        startTime: currentTime,
+        count: 1,
+      };
+    } else {
+      rateLimitStore[req.ip].count++;
+      if (rateLimitStore[req.ip].count > numOfRequest) {
+        return res.status(429).json({ error: "Too many request" });
+      }
+    }
+    console.log(rateLimitStore);
+    next();
+  };
+}
+
+app.get(
+  "/register",
+  rateLimiter({ windowSize: 100000, numOfRequest: 10 }),
+  async (req, res) => {
+    bcrypt.hashSync("123456", 14);
+    return res.json({ message: "Registered Successfully" });
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`🚀 Visit http://localhost:${PORT}`);
