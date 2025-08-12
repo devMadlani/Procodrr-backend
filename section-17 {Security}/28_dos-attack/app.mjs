4516a9158816100069f3decd9e32f5c2781362b6
Dev Madlani <madlanidev@gmail.com>
2025-08-12 19:27:32 +0530

    used express rate-limit

--- section-17 {Security}/28_dos-attack/app.mjs
+++ section-17 {Security}/28_dos-attack/app.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import bcrypt from "bcrypt";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
 const PORT = 4000;
@@ -9,41 +10,56 @@ app.use((req, res, next) => {
   next();
 });
 
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
+app.use(globalLimiter);
+
+const registerLimiter = rateLimit({
+  windowMs: 5000,
+  limit: 2,
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-const rateLimitStore = {};
+// const rateLimitStore = {};
 
-function rateLimiter({ windowSize, numOfRequest }) {
-  return function (req, res, next) {
-    const currentTime = Date.now();
-    if (!rateLimitStore[req.ip]) {
-      rateLimitStore[req.ip] = {
-        startTime: currentTime,
-        count: 1,
-      };
-      return next();
-    }
-    if (currentTime - rateLimitStore[req.ip].startTime > windowSize) {
-      rateLimitStore[req.ip] = {
-        startTime: currentTime,
-        count: 1,
-      };
-    } else {
-      rateLimitStore[req.ip].count++;
-      if (rateLimitStore[req.ip].count > numOfRequest) {
-        return res.status(429).json({ error: "Too many request" });
-      }
-    }
-    console.log(rateLimitStore);
-    next();
-  };
-}
+// function rateLimiter({ windowSize, numOfRequest }) {
+//   return function (req, res, next) {
+//     const currentTime = Date.now();
+//     if (!rateLimitStore[req.ip]) {
+//       rateLimitStore[req.ip] = {
+//         startTime: currentTime,
+//         count: 1,
+//       };
+//       return next();
+//     }
+//     if (currentTime - rateLimitStore[req.ip].startTime > windowSize) {
+//       rateLimitStore[req.ip] = {
+//         startTime: currentTime,
+//         count: 1,
+//       };
+//     } else {
+//       rateLimitStore[req.ip].count++;
+//       if (rateLimitStore[req.ip].count > numOfRequest) {
+//         return res.status(429).json({ error: "Too many request" });
+//       }
+//     }
+//     console.log(rateLimitStore);
+//     next();
+//   };
+// }
 
 app.get(
   "/register",
-  rateLimiter({ windowSize: 100000, numOfRequest: 10 }),
+  // rateLimiter({ windowSize: 100000, numOfRequest: 10 }),
+  registerLimiter,
   async (req, res) => {
     bcrypt.hashSync("123456", 14);
     return res.json({ message: "Registered Successfully" });
