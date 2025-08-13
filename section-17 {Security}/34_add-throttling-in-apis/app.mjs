ecfe319eec99c1375696ed4ccb921c665da71a01
Dev Madlani <madlanidev@gmail.com>
2025-08-13 08:48:14 +0530

    used throttling in apis

---
+++ section-17 {Security}/34_add-throttling-in-apis/app.mjs
@@ -0,0 +1,53 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import { rateLimit } from "express-rate-limit";
+
+const app = express();
+const PORT = 4000;
+
+const limiter = rateLimit({
+  windowMs: 20000,
+  limit: 5,
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+});
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+function throttle(waitTime = 1000) {
+  const throttleData = {};
+
+  return (req, res, next) => {
+    const now = Date.now();
+    const { previousDelay, lastReqTime } = throttleData[req.ip] || {
+      previousDelay: 0,
+      lastReqTime: now - waitTime,
+    };
+
+    const timePassed = now - lastReqTime;
+    const delay = Math.max(0, waitTime + previousDelay - timePassed);
+
+    throttleData[req.ip] = {
+      lastReqTime: now,
+      previousDelay: delay,
+    };
+    setTimeout(next, delay);
+  };
+}
+app.use(limiter, throttle(2000));
+
+app.get("/", throttle(2000), (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/register", async (req, res) => {
+  bcrypt.hashSync("123456", 14);
+  return res.json({ message: "Registered Successfully" });
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Visit http://localhost:${PORT}`);
+});
